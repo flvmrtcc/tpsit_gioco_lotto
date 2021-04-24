54746e0577b2c2b8429255c679676e6209059baa
flvmrtcc <flavio.martucci.fm@gmail.com>
2021-04-24 16:09:26 +0200

    Controllo giocatore maggiorenne

--- gioco_lotto.py
+++ gioco_lotto.py
@@ -1,22 +1,50 @@
+from datetime import date
 from codicefiscale import build
 import codicefiscale
 
 def inserimentoCodiceFiscale():
-    codiceFiscale = 0
-    while not controllaValiditaCodiceFiscale(codiceFiscale):
-        codiceFiscale = input("Inserisci il codice fiscale:")
-    return codiceFiscale
+    codice_fiscale = 0
+    valido = False
+    while not valido:
+        codice_fiscale = input("Inserisci il codice fiscale:")
+        valido = controllaValiditaCodiceFiscale(codice_fiscale)
+    return codice_fiscale
 
-def controllaValiditaCodiceFiscale(codiceFiscale):
-    if(codicefiscale.isvalid(codiceFiscale)):
+def controllaValiditaCodiceFiscale(codice_fiscale):
+    if(codicefiscale.isvalid(codice_fiscale)):
         print("Il codice fiscale inserito è valido.")
         return True
     else:
         print("Il codice fiscale inserito non è valido.")
         return False
 
-codiceFiscale = inserimentoCodiceFiscale()
-print(f"Il codice fiscale inserito è: {codiceFiscale}")
 
+def verficaSeMaggiorenne(codice_fiscale):
+    dataNascita = codicefiscale.get_birthday(codice_fiscale)
+    giorno_codice = dataNascita[0:2]
+    mese_codice = dataNascita[3:5]
+    anno_codice = dataNascita[6:9]
+    if int(anno_codice) < 25:   # 2025
+        anno = f"20{anno_codice}"
+    else:
+        anno = f"19{anno_codice}"
+    print(giorno_codice + " " + mese_codice + " " + anno)
+    data_attuale = date.today()
 
+    if (data_attuale.year - int(anno)) > 18:
+        return True
+    elif (data_attuale.year - int(anno)) == 18:
+        if data_attuale.month >= int(mese_codice):
+            if data_attuale.day >= int(giorno_codice):
+                return True
+    return False
+
+print("Benvenuto nel gioco del lotto!")
+codice_fiscale = inserimentoCodiceFiscale()
+# print(f"Il codice fiscale inserito è: {codice_fiscale}")
+
+if verficaSeMaggiorenne(codice_fiscale):
+    print("è maggiorenne")
+else:
+    print("non è maggiorenne")
 
